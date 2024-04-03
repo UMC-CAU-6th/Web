3a3ab3a75fc8769ade573ddd31c93cfacfe644cb
selfishAltruism <officialkyus@gmail.com>
2024-04-03 16:54:12 +0900

    feat: todolist compolite

--- 1_week_mission/signUp/src/script.js
+++ 1_week_mission/signUp/src/script.js
@@ -1,94 +1,73 @@
 const form = document.getElementById("registrationForm");
+const modal = document.querySelector(".modal-wrapper");
+const close = document.getElementById("close");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault(); // Prevent default form submission'
   const name = document.getElementById("name").value;
   const email = document.getElementById("email").value;
-  const age = document.getElementById("age").value;
+  const age = +document.getElementById("age").value;
   const password = document.getElementById("password").value;
   const confirmPassword = document.getElementById("confirmPassword").value;
 
   // Validation
   let isValid = true;
   if (!validateEmail(email)) {
-    displayError("emailError", "Invalid email address");
+    displayError("emailError", "이메일 형식이 안 맞아요.");
     isValid = false;
   } else {
     clearError("emailError");
   }
 
   if (!validateAge_integer(age)) {
-    displayError("ageError", "Age must be a positive integer greater than 18");
+    console.log(Number.isInteger(age));
+    displayError("ageError", "나이가 정수가 아니면 어뜨케요.");
     isValid = false;
-  } else {
-    clearError("ageError");
-  }
-
-  if (!validateAge_parseInt(age)) {
-    displayError("ageError", "Age must be a positive integer greater than 18");
+  } else if (!validateAge_parseInt(age)) {
+    displayError("ageError", "나이가 음수라고요?");
     isValid = false;
-  } else {
-    clearError("ageError");
-  }
-
-  if (!validateAge_min(age)) {
-    displayError("ageError", "Age must be a positive integer greater than 18");
+  } else if (!validateAge_min(age)) {
+    displayError("ageError", "너는 발 닦고 자라");
     isValid = false;
   } else {
     clearError("ageError");
   }
 
   if (!validatePassword(password)) {
-    displayError(
-      "passwordError",
-      "Password must be between 4 and 12 characters long and contain English letters, numbers, and special characters."
-    );
+    displayError("passwordError", "비밀번호가 너무 약해요.");
     isValid = false;
-  } else {
-    clearError("passwordError");
-  }
-
-  if (!validatePassword_min(password)) {
-    displayError(
-      "passwordError",
-      "Password must be between 4 and 12 characters long and contain English letters, numbers, and special characters."
-    );
+  } else if (!validatePassword_min(password)) {
+    displayError("passwordError", "비밀번호가 너무 짧아요.");
     isValid = false;
-  } else {
-    clearError("passwordError");
-  }
-
-  if (!validatePassword_max(password)) {
-    displayError(
-      "passwordError",
-      "Password must be between 4 and 12 characters long and contain English letters, numbers, and special characters."
-    );
+  } else if (!validatePassword_max(password)) {
+    displayError("passwordError", "비밀번호가 너무 길어요.");
     isValid = false;
   } else {
     clearError("passwordError");
   }
 
   if (password !== confirmPassword) {
-    displayError("confirmPasswordError", "Passwords do not match");
+    displayError("confirmPasswordError", "입력한 비밀번호랑 다르잖아요.");
     isValid = false;
   } else {
     clearError("confirmPasswordError");
   }
 
   if (isValid) {
-    console.log("Name:", name);
-    console.log("Email:", email);
-    console.log("Age:", age);
-    console.log("Password:", password);
+    modal.style.display = "flex";
   }
 });
+
+close.onclick = () => {
+  modal.style.display = "none";
+};
 
 const validateEmail = (email) => {
   return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
 };
 
 const validateAge_integer = (age) => {
-  return Number.isInteger();
+  return Number.isInteger(age);
 };
 
 const validateAge_parseInt = (age) => {
@@ -100,9 +79,7 @@ const validateAge_min = (age) => {
 };
 
 const validatePassword = (password) => {
-  return /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]$/.test(
-    password
-  );
+  return /^(?=.*?[a-zA-Z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-])$/.test(password);
 };
 
 const validatePassword_min = (password) => {
