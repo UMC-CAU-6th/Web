09d6a5631588602ca194d99f09e5a7d427eb0026
juuuuone <jjw9597@gmail.com>
2024-04-23 12:27:15 +0900

    refactor: 폴더 구조 변경

---
+++ paeng/Week2/Mission2/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+import Movies from "./components/Movies";
+
+function App() {
+  return (<>
+  <Movies/>
+
+  </>
+  );
+}
+
+export default App;
