6a7a3dc9523ad74014a9e7f3b146d618a7b175be
juuuuone <jjw9597@gmail.com>
2024-05-01 13:14:22 +0900

    FEAT : Create movie page using TMDB API

---
+++ paeng/Week3/Mission/src/components/Navbar.jsx
@@ -0,0 +1,100 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const NavContainer = styled.div`
+  background-color: #1e174e;
+  color: white;
+  display: flex;
+  justify-content: space-between;
+  padding: 1rem;
+  margin: 0;
+`;
+
+const LogoLink = styled(Link)`
+  background-color: #1e174e;
+  color: white;
+  flex-shrink: 0;
+  display: inline;
+  cursor: pointer;
+  text-decoration: none;
+`;
+
+const NavBlock = styled.div`
+  background-color: #1e174e;
+  color: white;
+  display: flex;
+  padding: 1rem;
+  margin: 0;
+  justify-content: flex-end;
+`;
+
+const StyledLink = styled(Link)`
+  font-size: 1.125rem;
+  cursor: pointer;
+  white-space: pre;
+  padding-bottom: 0.25rem;
+
+  &:visited,
+  &:link,
+  &:active,
+  &:hover {
+    color: white;
+    text-decoration: none;
+  }
+
+  &:hover {
+    font-size: 1.2rem;
+    font-weight: bold;
+  }
+
+  & + & {
+    margin-left: 1rem;
+  }
+  
+`;
+
+const categories = [
+  {
+    name: "join",
+    text: "회원가입",
+    path: "/join",
+  },
+  {
+    name: "popular",
+    text: "Popular",
+    path: "/popular",
+  },
+  {
+    name: "nowPlaying",
+    text: "Now playing",
+    path: "/now-playing",
+  },
+  {
+    name: "topRated",
+    text: "Top Rated",
+    path: "/top-rated",
+  },
+  {
+    name: "upcoming",
+    text: "UpComing",
+    path: "/upcoming",
+  },
+];
+
+const Navbar = () => {
+  return (
+    <NavContainer>
+        <LogoLink className="Logo" to="/">UMC MOVIE</LogoLink>
+
+      <NavBlock>
+        {categories.map((c) => (
+          <StyledLink to={c.path} key={c.name}>
+            {c.text}
+          </StyledLink>
+        ))}
+      </NavBlock>
+    </NavContainer>
+  );
+};
+
+export default Navbar;
