8969b31d3f2183b178f2605676eee5976583d615
selfishAltruism <officialkyus@gmail.com>
2024-05-02 19:40:23 +0900

    design: complite navbar

--- mingky/3_week_mission/src/App.tsx
+++ mingky/3_week_mission/src/App.tsx
@@ -1,6 +1,7 @@
 import { Suspense, lazy } from "react";
 import { BrowserRouter as RootRouter, Routes, Route } from "react-router-dom";
 
+import Navbar from "./component/Navbar";
 import Loading from "./component/Loading";
 
 const MovicePosterGrid = lazy(() => import("./component/MovicePosterGrid"));
@@ -8,6 +9,7 @@ const MovicePosterGrid = lazy(() => import("./component/MovicePosterGrid"));
 const App = () => {
   return (
     <Suspense fallback={<Loading />}>
+      <Navbar />
       <RootRouter>
         <Routes>
           <Route path={"/movices"} element={<MovicePosterGrid />} />
--- mingky/3_week_mission/src/component/Navbar.tsx
+++ mingky/3_week_mission/src/component/Navbar.tsx
@@ -1,14 +1,49 @@
 import styled from "@emotion/styled";
 
-const Navbar = () => {};
+const Navbar = () => {
+  return (
+    <Container>
+      <MainButton>UMC Movie</MainButton>
+      <Button>Popular</Button>
+      <Button>Now Playing</Button>
+      <Button>Top Rated</Button>
+      <Button>Up Coming</Button>
+    </Container>
+  );
+};
 
 const Container = styled.div`
+  position: fixed;
+
+  top: 0px;
+  left: 0px;
+
+  z-index: 3;
+
   display: flex;
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
+
+  width: 100%;
+  height: 50px;
+
+  background-color: #000a7d;
 `;
 
-const Button = styled.button``;
+const Button = styled.button`
+  border: 0px;
+
+  background-color: #000a7d;
+
+  font-size: 20px;
+  color: white;
+`;
+
+const MainButton = styled(Button)`
+  font-size: 30px;
+
+  margin-right: 800px;
+`;
 
 export default Navbar;
