de7a06f21bcdab35f47cef66d7fce8b213eb2928
selfishAltruism <officialkyus@gmail.com>
2024-05-02 19:32:53 +0900

    chore: create navber, home component

---
+++ mingky/3_week_mission/src/component/Navbar.tsx
@@ -0,0 +1,14 @@
+import styled from "@emotion/styled";
+
+const Navbar = () => {};
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Button = styled.button``;
+
+export default Navbar;
---
+++ mingky/3_week_mission/src/page/home.tsx
@@ -0,0 +1,5 @@
+import styled from "@emotion/styled";
+
+const Home = () => {};
+
+export default Home;
