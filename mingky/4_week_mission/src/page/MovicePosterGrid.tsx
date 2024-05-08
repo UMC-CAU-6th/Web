6e4981c2a1e16faa416605878e779d07982562ad
selfishAltruism <officialkyus@gmail.com>
2024-05-08 09:43:28 +0900

    feat: add 4 week mission project

---
+++ mingky/4_week_mission/src/page/MovicePosterGrid.tsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+import "../style/MovicePosterGridStyles.css";
+
+import { getMovies } from "../service/MovicePosterService";
+import { Movies, Movie } from "../types";
+import MovicePoster from "../component/MovicePoster";
+
+const options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: import.meta.env.VITE_SERVER_URL,
+  },
+};
+
+const MovicePosterGrid = ({ url }: { url: string }) => {
+  const [movies, setMovies] = useState<Movies>({
+    page: 0,
+    results: [],
+    total_pages: 0,
+    total_results: 0,
+  });
+
+  useEffect(() => {
+    fetch(url, options)
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+        return response;
+      })
+      .then((response) => {
+        setMovies(response);
+      })
+      .catch((err) => console.error(err));
+  }, [url]);
+
+  return (
+    <div className="poster-grid">
+      {movies.results.map((movie: Movie) => (
+        <MovicePoster key={movie.id} {...movie} />
+      ))}
+    </div>
+  );
+};
+
+export default MovicePosterGrid;
