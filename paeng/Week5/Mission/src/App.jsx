238f2dd92d2029b13c82db4262fe53041ba70230
juuuuone <jjw9597@gmail.com>
2024-05-16 15:32:24 +0900

    feat: 회원가입 유효성 검사, 검색 기능 추가chore: 폴더 구조 변경

---
+++ paeng/Week5/Mission/src/App.jsx
@@ -0,0 +1,39 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./common/Navbar";
+import Footer from "./common/Footer";
+import NowPlayingPage from "./pages/NowPlayingPage";
+import MainPage from "./pages/MainPage";
+import PopularPage from "./pages/PopularPage";
+import UpComing from "./pages/UpComing";
+import TopRatedPage from "./pages/TopRatedPage";
+import MovieDetail from "./components/MovieDetail";
+import NotFound from "./pages/NotFound";
+import SignupPage from "./pages/SignupPage";
+import Search from "./components/Search";
+
+const App = () => {
+  return (
+    <BrowserRouter> 
+      <Navbar />
+      <Routes>
+        <Route path="/top-rated" element={<TopRatedPage/>}/>
+        <Route path="/now-playing" element={<NowPlayingPage/>}/>
+        <Route path="/popular" element={<PopularPage/>}/>
+        <Route path="/upcoming" element={<UpComing/>}/>
+        <Route path="/" element={<MainPage/>}/>
+        <Route path="/signup" element={<SignupPage />}/>
+        <Route path="/movie/detail/:original_title" element={<MovieDetail/>}/>
+        <Route path="*" element={<NotFound />}/>
+      </Routes>
+      
+      <Footer /> 
+    </BrowserRouter>
+  
+  );
+};
+
+export default App;
+
+//브라우저 라우터는 라우트를 쓰는 모든곳을 커버
+//Routes를 쓰면 ... 그 안에껏만 바뀜
+//Route
---
+++ paeng/Week5/Mission/src/pages/JoinPage.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const JoinBlock = styled.div`
+  height: 100vh;
+  width: 100vw;
+`;
+
+const JoinPage = () => {
+  return <JoinBlock/>;
+};
+
+export default JoinPage;
